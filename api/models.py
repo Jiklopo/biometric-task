1c94013966671819c6ac86eac9cf2b628cac1a7c
Timur <t_khaimovich@outlook.com>
2021-07-12 18:52:19 +0600

    upd

--- api/models.py
+++ api/models.py
@@ -1,4 +1,3 @@
-from .validators import validate_iin
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator, EmailValidator
 from .validators import validate_iin, not_future
--- biometric_task/settings.py
+++ biometric_task/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8z_n@h2(&zwzx@zxdwijg*renyx#t0i+(daw-(me$4dy1b+0wc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
