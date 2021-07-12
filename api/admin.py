d78767979f70d50befeea0253a707daed9fbb825
Timur <t_khaimovich@outlook.com>
2021-07-12 20:07:21 +0600

    added models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Staff, Restaurant, Pizza, Person
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Staff)
+admin.site.register(Pizza)
+admin.site.register(Person)
