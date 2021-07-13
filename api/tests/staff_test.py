afb9f70746ee2813bd83330a0add666e9288c8a8
Timur <t_khaimovich@outlook.com>
2021-07-13 11:41:42 +0600

    refactored

--- api/tests/staff_test.py
+++ api/tests/staff_test.py
@@ -5,21 +5,19 @@ import datetime as dt
 
 
 def test_email(create_staff):
-    staff = create_staff
-    staff.email = 'bad_really'
+    create_staff.email = 'bad_really'
     with pytest.raises(ValidationError):
-        staff.full_clean()
+        create_staff.full_clean()
 
 
 def test_birth_date(create_staff):
-    staff = create_staff
-    staff.birth_date = dt.date.today() + dt.timedelta(days=1)
+    create_staff
+    create_staff.birth_date = dt.date.today() + dt.timedelta(days=1)
     with pytest.raises(ValidationError):
-        staff.full_clean()
+        create_staff.full_clean()
 
 
 def test_date_joined(create_staff):
-    staff = create_staff
-    staff.date_joined = dt.date.today() + dt.timedelta(days=1)
+    create_staff.date_joined = dt.date.today() + dt.timedelta(days=1)
     with pytest.raises(ValidationError):
-        staff.full_clean()
+        create_staff.full_clean()
