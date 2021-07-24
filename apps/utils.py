631a61282362dbd39a41bc108ee9d0d173d2b7e3
Timur <t_khaimovich@outlook.com>
2021-07-24 13:23:26 +0600

    refactored utils

--- apps/utils.py
+++ apps/utils.py
@@ -4,7 +4,6 @@ from django.db.models import Model
 def update_model(*, model: Model, **data):
     for field_name, field_value in data.items():
         if hasattr(model, field_name):
-            if field_value:
-                setattr(model, field_name, field_value)
+            setattr(model, field_name, field_value)
     model.save()
     return model
