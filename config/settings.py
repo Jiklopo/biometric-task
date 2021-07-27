2abf7b4fb7b5a6804f89dcbf97a74017d50a8897
Timur <t_khaimovich@outlook.com>
2021-07-27 11:08:13 +0600

    fixed redis path for docker

--- config/settings.py
+++ config/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
